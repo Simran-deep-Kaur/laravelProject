8c0e14ee64f06cceec317ab9823f53fc041defc9
SimranCraft <simrandeep.kaur@canvascraft.mediagit config --global user.email simrandeep.kaur@canvascraft.media>
2024-04-03 12:09:51 +0530

    committedData

--- resources/views/admins/create.blade.php
+++ resources/views/admins/create.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Employees') }}
+            {{ __('Add Admin') }}
         </h2>
     </x-slot>
 
     <div class="max-w-2xl mx-auto py-6 sm:px-6 lg:px-8">
         <form enctype="multipart/form-data"  action="{{route('admin.store')}}" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             @csrf
-            @include ('common.form')
+            @include ('common.form',['url' => route('admins.checkEmail')])
         </form>
     </div>
 </x-app-layout>
